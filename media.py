7557cc003de2b4b267721a87a9b54a2be7c1c7f3
Adeyinka Adegbenro <30419309+bearteam15@users.noreply.github.com>
2017-09-14 08:44:21 +0100

    removing redundant files

--- media.py
+++
@@ -1,8 +0,0 @@
-
-class Movie(object):
-    """This class stores movie related information \
-    such as movie title, poster image url, and the movie's youtube url"""
-    def __init__(self, movie_title, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
